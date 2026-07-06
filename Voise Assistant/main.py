5b24492154a9bfcafccdd65ec066473187a320dd
krish14-ai <kkhandelwal292@gmail.com>
2026-07-06 20:54:28 +0530

    done for today

--- Voise Assistant/main.py
+++ Voise Assistant/main.py
@@ -2,7 +2,23 @@ import speech_recognition as sr
 import webbrowser as wb
 import datetime as dt
 import pyjokes as pj
-import pyttsx3 
+import pyttsx3
+import os
+
+##-------------------------------------
+## initializing voice engine 
+engine = pyttsx3.init()
+
+## Setting Voice, 0 = female, 1 = male
+voice = engine.getProperty('voices')
+engine.setProperty('voice',voice[1].id)
+
+## Setting rate of Speech
+rate = engine.getProperty('rate')
+engine.setProperty('rate',150)
+
+##------------------------------------
+
 
 def sptext():
     recognizer = sr.Recognizer()
@@ -20,17 +36,49 @@ def sptext():
 
 
 def txt_to_speech(text):
-    engine = pyttsx3.init()
-    ## Setting Voice, 0 = female, 1 = male
-    voice = engine.getProperty('voices')
-    engine.setProperty('voice',voice[0].id)
 
-    rate = engine.getProperty('rate')
-    engine.setProperty('rate',100)
-
-    engine.say(X)
+    ## to speak
+    engine.say(text)
+    engine.runAndWait()
 
 
 
-data = sptext()
-print(data)
+if __name__ == '__main__':
+    name = 'hello! my name is jarvis'
+    txt_to_speech(name)
+    
+    greeting = "how may i help you today"
+    print(greeting)
+
+    txt_to_speech(greeting)
+    dt.sleep(2)
+    command = ''
+
+    while True:
+        command = sptext()
+
+        if command is None:
+            continue
+
+        command = command.lower()
+
+        if "good bye" in command:
+            txt_to_speech("Goodbye!")
+            break
+
+        elif "time" in command:
+            current_time = dt.datetime.now().strftime("%I:%M %p")
+            txt_to_speech(f"The time is {current_time}")
+
+        elif "date" in command:
+            current_date = dt.datetime.now().strftime("%d %B %Y")
+            txt_to_speech(f"Today's date is {current_date}")
+
+        elif "tell me a joke" in command:
+            joke = pj.get_joke(language='en')
+            txt_to_speech(joke)
+        elif "youtube" in command:
+            wb.open('https://www.youtube.com')
+        
+        else:
+            txt_to_speech(command)
